3bfecb62d0630bfe6f50cfece8ca032cbbdc5b05
krower345 <karinarower@gmail.com>
2025-12-05 02:43:47 -0500

    display current date and time in booking
    Added current date and time display for booking.

--- book.js
+++ book.js
@@ -14,7 +14,36 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 1000); // Change image every 2 seconds
+  setTimeout(showSlides, 1000);
 }
 
 // Generate date and time for booking
+
+const time = new Date();
+document.getElementById("current-date").innerHTML = time;
+
+// Live update receipt (struggled to get it to work using JS, commenting it out until next report)
+  // const STARTING = 15.0;
+  // const selectCats = $('#cats');
+  // const subtotalEl = $('#subtotal');
+  // const multipliedEl = $('#multiply-math');
+  // const startingEl = $('#starting');
+  // const finalEl = $('#final');
+  // const itemsList = $('#items-list');
+  // const bookingForm = document.querySelector('form.auth-form');
+  // const currentDateEl = document.querySelector('.current-date');
+  // const checkedCat = form.querySelector('option[value="1"]:checked');
+ 
+  // function calculateReceipt() {
+    //const selected = Array.from(selectCats.selectedOptions);
+    // }
+
+   // const multiplied = subtotal * 5;
+    // const finalTotal = STARTING + multiplied;
+
+    // Update UI
+   // subtotalEl.textContent = fmt(subtotal);
+   // multipliedEl.textContent = fmt(multiplied);
+    // finalEl.textContent = fmt(finalTotal);
+
+
